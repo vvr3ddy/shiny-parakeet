480afcd14e3774f817f4a4a3104022b48c41f1c2
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-17 21:40:48 +0530

    :octocat: Refactor some bot logic
    * We're live :P, slang lang to dev code.
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/admin/filters.ts
+++ src/admin/filters.ts
@@ -6,6 +6,7 @@ export const filters = new Composer();
 filters.command("trigger", async (ctx) => {
     try {
         const args = ctx.match.split(" ");
+        // If there are no args passed, throw an error.
         if (args[0] === "") throw new Error('No args passed!');
         /*
         * The format for valid input would be a reply to a message with 
@@ -13,10 +14,12 @@ filters.command("trigger", async (ctx) => {
         * where 
         *       count = no of messages after which the bot responds
         *       message = message which the bot replies to the user
-        */  
-        bot.api.sendMessage(ctx.msg.chat.id, args.toString());
-        if(ctx.msg.reply_to_message==null) throw new Error('Reply to a message to use this feature!');
-        ctx.reply(`Message sending fukkr is: ${ctx.msg.reply_to_message?.from?.id}, who's basically ${ctx.msg.reply_to_message.from?.first_name} ${ctx.msg.reply_to_message.from?.last_name}`);
+        */
+        // Follow the proper template to send request
+        if (args.length !== 2) throw new Error('Invalid Format!');
+
+        if (ctx.msg.reply_to_message == null) throw new Error('Reply to a message to use this feature!');
+        ctx.reply(`Message sent by: ${ctx.msg.reply_to_message?.from?.id}, who's basically ${ctx.msg.reply_to_message.from?.first_name} ${ctx.msg.reply_to_message.from?.last_name}`);
     } catch (error) {
         if (error instanceof Error)
             await ctx.reply(error.message);
