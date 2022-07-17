5f662fd8f87337e6a3afa6d9171699bb02d5dd08
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-17 23:05:56 +0530

    :octocat: :gear: filters: Only allow valid format.
    * We are using a generic error for now. TODO: Implement a proper exception handling.
    * First check if the message is a reply or no.
    * Secondly, check if the format is proper /trigger <count> <message>
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/admin/filters.ts
+++ src/admin/filters.ts
@@ -5,7 +5,8 @@ export const filters = new Composer();
 
 filters.command("trigger", async (ctx) => {
     try {
-        const args = ctx.match.split(" ");
+        const arg_chunk = ctx.match.split(/\s+/);
+        const args = [arg_chunk.shift(), arg_chunk.join(" ")];
         // If there are no args passed, throw an error.
         if (args[0] === "") throw new Error('No args passed!');
         /*
@@ -18,7 +19,16 @@ filters.command("trigger", async (ctx) => {
         // Follow the proper template to send request
         if (args.length !== 2) throw new Error('Invalid Format!');
 
+        const count: number = parseInt(args[0]!);
+        const message: string = args[1]!.toString();
+        // The command must to be a reply to a message, because we fetch the target user based on this info.
         if (ctx.msg.reply_to_message == null) throw new Error('Reply to a message to use this feature!');
+        if (typeof count !== 'number' || typeof message !== 'string'){
+            throw new Error('Invalid Format!');
+        }
+        if(isNaN(count)){
+            throw new Error('Invalid Format!');
+        }
         ctx.reply(`Message sent by: ${ctx.msg.reply_to_message?.from?.id}, who's basically ${ctx.msg.reply_to_message.from?.first_name} ${ctx.msg.reply_to_message.from?.last_name}`);
     } catch (error) {
         if (error instanceof Error)
