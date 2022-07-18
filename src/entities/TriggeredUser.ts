22e6662fce67fc9ed0d65b7e58781c801485f9bd
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-18 17:57:34 +0530

    :octocat: :sparkles: Entities: Create TriggeredUser entity * Entity TriggeredUser 	- userName 	- userId 	- count 	- message 	- generate Getters and Setters 	- override toString
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ src/entities/TriggeredUser.ts
@@ -0,0 +1,42 @@
+export class TriggeredUser {
+    private _userName: string;
+    private _userId: number;
+    private _count: number;
+    private _message: string;
+    
+    constructor(userName: string, userId: number, count: number, message: string) {
+        this._userName = userName;
+        this._userId = userId;
+        this._count = count;
+        this._message = message;
+    }
+
+    public get userName(): string {
+        return this._userName;
+    }
+    public set userName(value: string) {
+        this._userName = value;
+    }
+    public get userId(): number {
+        return this._userId;
+    }
+    public set userId(value: number) {
+        this._userId = value;
+    }
+    public get count(): number {
+        return this._count;
+    }
+    public set count(value: number) {
+        this._count = value;
+    }
+    public get message(): string {
+        return this._message;
+    }
+    public set message(value: string) {
+        this._message = value;
+    }
+
+    public toString = (): string =>{
+        return `TriggeredUser (${this._userName}, ${this._userId}, ${this._count}, ${this._message})`;
+    }
+}
