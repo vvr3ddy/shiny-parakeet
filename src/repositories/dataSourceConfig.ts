c81e03f3740cadad235be87598d4e52ed3c12184
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2022-07-18 22:42:14 +0530

    :octocat: :gear: DataSource: port config
    Avoid overriding port.
    Give the user their choice of port.

--- src/repositories/dataSourceConfig.ts
+++ src/repositories/dataSourceConfig.ts
@@ -4,7 +4,7 @@ import { bot } from "../../bot";
 export const BotDataSource = new DataSource({
     type: "postgres",
     host: process.env["DBHOST"],
-    port: 5432,
+    port: process.env["DB_PORT"] || 5432,
     username: process.env["DB_USERNAME"],
     password: process.env["DB_PASSWORD"],
     database: process.env["DB_NAME"],
