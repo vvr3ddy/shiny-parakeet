97b0d04dae1cd6e1d4548965a6acf73070c3aaaa
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-18 22:12:04 +0530

    :octocat: Implement DataStore for ORM capabilities
    * Lets use postgres
    * make use of env vars for db connection props
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ src/repositories/dataSourceConfig.ts
@@ -0,0 +1,19 @@
+import { DataSource } from "typeorm";
+import { bot } from "../../bot";
+
+export const BotDataSource = new DataSource({
+    type: "postgres",
+    host: process.env["DBHOST"],
+    port: 5432,
+    username: process.env["DB_USERNAME"],
+    password: process.env["DB_PASSWORD"],
+    database: process.env["DB_NAME"],
+})
+
+BotDataSource.initialize()
+    .then(()=>{
+        console.log("Data Source has been initialized!")
+    })
+    .catch(async (err)=>{
+        console.error("Error during Data source Initialization", err);
+    })
