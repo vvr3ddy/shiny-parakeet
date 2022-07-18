9f189217b072a139a7013a027ff8b5deaa0af169
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-19 00:26:52 +0530

    :octocat: :bug: env-vars: expose DB vars
    Fix for issue #14
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- environment.d.ts
+++ environment.d.ts
@@ -6,6 +6,11 @@ declare global {
       BOT_API_KEY: string;
       ENV: 'test' | 'dev' | 'prod';
       OWNER: number;
+      DB_PORT: number;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+      DB_HOST: string;
     }
   }
 }
