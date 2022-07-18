c2519ac2bc2f45b5260b543036c8357ce62ff33b
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-18 19:09:50 +0530

    :octocat: :gear: filters: Store the details of user * Store the details of the user in a TriggeredUser object.
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/controllers/modules/filters.ts
+++ src/controllers/modules/filters.ts
@@ -1,5 +1,6 @@
 import { Composer } from "grammy";
 import { bot } from "../../../bot";
+import { TriggeredUser } from "../../entities/TriggeredUser";
 
 export const filters = new Composer();
 
@@ -7,8 +8,10 @@ filters.command("trigger", async (ctx) => {
     try {
         const arg_chunk = ctx.match.split(/\s+/);
         const args = [arg_chunk.shift(), arg_chunk.join(" ")];
+        
         // If there are no args passed, throw an error.
         if (args[0] === "") throw new Error('No args passed!');
+        
         /*
         * The format for valid input would be a reply to a message with 
         * /trigger <count> <message>
@@ -21,15 +24,25 @@ filters.command("trigger", async (ctx) => {
 
         const count: number = parseInt(args[0]!);
         const message: string = args[1]!.toString();
+        
         // The command must to be a reply to a message, because we fetch the target user based on this info.
         if (ctx.msg.reply_to_message == null) throw new Error('Reply to a message to use this feature!');
+        
         if (typeof count !== 'number' || typeof message !== 'string'){
             throw new Error('Invalid Format!');
         }
+        
         if(isNaN(count)){
             throw new Error('Invalid Format!');
         }
-        const botMessage = await ctx.reply(`Message sent by: ${ctx.msg.reply_to_message?.from?.id}, who's basically ${ctx.msg.reply_to_message.from?.first_name} ${ctx.msg.reply_to_message.from?.last_name}`);
+        
+        const userName = ctx.msg.from?.first_name+" "+ctx.msg.from?.last_name;
+        const userId: number = ctx.msg.reply_to_message?.from?.id!; //Null safety is being expected -> need to work.
+        
+        const triggeredUser: TriggeredUser = new TriggeredUser(userName,userId, count,message);
+        // TODO: Save the user to a storage space. Probably a JSON file, or a DB.
+
+        const botMessage = await ctx.reply(`Captured the following: ${triggeredUser}`);
         // TODO: use the editMessageText option to notify of the trigger saved successfully.
         await bot.api.editMessageText(ctx.msg.chat.id, botMessage.message_id, "Text Changed");
     } catch (error) {
