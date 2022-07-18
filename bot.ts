39b920302b56668819201747853e1298f757ca75
vvr3ddy <vvr-rockstar@bmsit.in>
2022-07-18 20:33:04 +0530

    :octocat: :sparkles: Introduce ORM
    * Add TypeORM, mysql2, reflect-metadata and @types/node dependencies
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- bot.ts
+++ bot.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 import { Bot } from "grammy";
 import { helpers } from "./src/controllers/admin/helpers";
 import { filters } from "./src/controllers/modules/filters"
+import "reflect-metadata"
 
 // Fetch the bot api key from env vars
 let BOT_API_KEY = process.env["BOT_API_KEY"];
